f93b146adf89164054bc170f16943515ce26fb36
GitHub Action <action@github.com>
2022-12-23 00:58:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/cpoile/loki/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cpoile/loki/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cpoile.loki.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cpoile.loki.MyBundle
 
 class MyApplicationService {
 
